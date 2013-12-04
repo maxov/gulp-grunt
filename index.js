4f4152c067ef1d0e9bd92c48beeddad8dcb37ee7
gratimax <max@ovsankin.com>
2013-12-03 20:40:06 -0800

    split tasks out to separate function

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
 var grunt = require('grunt')
 
 module.exports = function(gulp, options) {
-
     opt = {
         base: null,
         prefix: 'grunt-'
     }
-
     if(options) {
         for(var key in options) {
             opt[key] = options[key]
         }
     }
-
     if(opt.base) {
         grunt.file.setBase(opt.base)
     }
+}
 
+module.exports.tasks = function(options) {
     grunt.task.init([])
-    var tasks = grunt.task._tasks
 
-    for(var name in tasks) {
-        var task = tasks[name]
-        gulp.task(opt.prefix + name, task.fn)
+    var gruntTasks = grunt.task._tasks,
+        finalTasks = {}
+
+    for(var name in gruntTasks) {
+        var task = gruntTasks[name]
+        finalTasks[opt.prefix + name] = task.fn
     }
 
+    return finalTasks;
 }
